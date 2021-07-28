1d1c50c31a7163ff9e044919054ff4625729bab6
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-28 12:43:52 -0300

    realicé las primeras configuraciones y maquetado de navbars top y bottom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header/Header";
+import MainNav from "./components/MainNav"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header></Header>
+      <div className="App">Hola mundo</div>
+      <MainNav></MainNav>
+    </>
   );
 }
 
