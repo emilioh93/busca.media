cfc7fc98f0fdf9f7ab06a65892427d852480814e
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-29 11:28:12 -0300

    agregué modal de las cards

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -2,6 +2,7 @@ import { Badge } from "@material-ui/core";
 import React from "react";
 import { img_300, unavailable } from "../../config/config";
 import "./SingleContent.css";
+import ContentModal from "../ContentModal/ContentModal";
 
 const SingleContent = ({
   id,
@@ -12,7 +13,7 @@ const SingleContent = ({
   vote_average,
 }) => {
   return (
-    <div className="media">
+    <ContentModal media_type={media_type} id={id}>
       <Badge
         badgeContent={vote_average}
         color={vote_average > 6 ? "primary" : "secondary"}
@@ -27,7 +28,7 @@ const SingleContent = ({
         <span>{media_type === "tv" ? "TV Series" : "Movie"}</span>
         <span>{date}</span>
       </div>
-    </div>
+    </ContentModal>
   );
 };
 
