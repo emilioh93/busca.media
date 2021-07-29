13f42823bfbc057ca69122b34ee334066ed37201
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-29 17:26:44 -0300

    agregué botón mi lista

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./Header.css";
+import { Container } from "@material-ui/core";
 
 const Header = () => {
   return (
-    <span onClick={() => window.scroll(0, 0)} className="header">
-      🎬 Entertainment Hub 🎥
-    </span>
+    <div className="header">
+      <Container>
+        <div className="header_container">
+          <div>
+            <span className="header_logo" onClick={() => window.scroll(0, 0)}>
+              🎬 Entertainment Hub 🎥
+            </span>
+          </div>
+          <div>
+            <span className="header_myList">My List</span>
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 };
 
