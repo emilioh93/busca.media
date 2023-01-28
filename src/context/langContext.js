deffd859d1a25154e445a1bc9ada722b0f0cc26a
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2023-01-28 15:06:31 -0300

    se mejoró code

--- src/context/langContext.js
+++ src/context/langContext.js
@@ -1,61 +1,43 @@
-import React, { useState } from "react";
-import Spanish from "./../lang/es-ES.json";
-import English from "./../lang/en-US.json";
-import { IntlProvider } from "react-intl";
+import React, { useState, useEffect, useMemo } from 'react';
+import { IntlProvider } from 'react-intl';
 
-const langContext = React.createContext();
+const languages = {
+  'en-US': { messages: require('./../lang/en-US.json') },
+  'es-ES': { messages: require('./../lang/es-ES.json') },
+};
+
+const LangContext = React.createContext();
 
 const LangProvider = ({ children }) => {
-  let localePorDefecto;
-  let mensajesPorDefecto;
+  const [local, setLocal] = useState('en-US');
+  const [messages, setMessages] = useState(languages['en-US'].messages);
 
-  const lang = localStorage.getItem("lang");
-
-  console.log({lang});
-
-  if (lang) {
-    localePorDefecto = lang;
-
-    if (lang === "es-ES") {
-      mensajesPorDefecto = Spanish;
-    } else if (lang === "en-US") {
-      mensajesPorDefecto = English;
-    } else {
-      localePorDefecto = "en-US";
-      mensajesPorDefecto = English;
+  useEffect(() => {
+    const storedLang = localStorage.getItem('lang');
+    if (storedLang && languages[storedLang]) {
+      setLocal(storedLang);
+      setMessages(languages[storedLang].messages);
     }
-  }
+  }, []);
 
-  const [mensajes, establecerMensajes] = useState(mensajesPorDefecto);
-  const [locale, establecerLocale] = useState(localePorDefecto);
-
-  const establecerLenguaje = (lenguaje) => {
-    switch (lenguaje) {
-      case "es-ES":
-        establecerMensajes(Spanish);
-        establecerLocale("es-ES");
-        localStorage.setItem("lang", "es-ES");
-        break;
-      case "en-US":
-        establecerMensajes(English);
-        establecerLocale("en-US");
-        localStorage.setItem("lang", "en-US");
-        break;
-
-      default:
-        establecerMensajes(English);
-        establecerLocale("en-US");
-        localStorage.setItem("lang", "en-US");
-    }
-  };
+  const setLanguage = useMemo(
+    () => language => {
+      if (languages[language]) {
+        setLocal(language);
+        setMessages(languages[language].messages);
+        localStorage.setItem('lang', language);
+      }
+    },
+    []
+  );
 
   return (
-    <langContext.Provider value={{ establecerLenguaje: establecerLenguaje }}>
-      <IntlProvider locale={locale} messages={mensajes}>
+    <LangContext.Provider value={{ setLanguage }}>
+      <IntlProvider local={local} messages={messages}>
         {children}
       </IntlProvider>
-    </langContext.Provider>
+    </LangContext.Provider>
   );
 };
 
-export { langContext, LangProvider };
+export { LangContext, LangProvider };
